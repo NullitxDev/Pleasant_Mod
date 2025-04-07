6d462b2ef438366f5c403758ba13a1df61b395b2
NullitxDev <dustypleasant68@gmail.com>
2025-04-07 04:23:25 -0400

    added springium

---
+++ src/main/java/net/nullitx/tutorialmod/Item/ModItems.java
@@ -0,0 +1,19 @@
+package net.nullitx.tutorialmod.Item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import net.nullitx.tutorialmod.TutorialMod;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, TutorialMod.MOD_ID);
+
+    public static final RegistryObject<Item> SPRINGIUM = ITEMS.register("springium", () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
