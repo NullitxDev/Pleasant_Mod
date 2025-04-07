dc403c0a42e6b3fcbf6045cff9b0a616fed45991
NullitxDev <dustypleasant68@gmail.com>
2025-04-07 03:41:38 -0400

    mods toml changed

--- src/main/java/net/nullitx/tutorialmod/TutorialMod.java
+++ src/main/java/net/nullitx/tutorialmod/TutorialMod.java
@@ -18,7 +18,7 @@ import org.slf4j.Logger;
 public class TutorialMod
 {
     // Define mod id in a common place for everything to reference
-    public static final String MOD_ID = "Nullitx_Tutorial_Mod";
+    public static final String MOD_ID = "nullitx_tutorial_mod";
     // Directly reference a slf4j logger
     private static final Logger LOGGER = LogUtils.getLogger();
 
